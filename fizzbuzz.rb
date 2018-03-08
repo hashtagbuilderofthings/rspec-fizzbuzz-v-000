b53f98377723490bbb95e013f6033724b3630d50
Ian  Jenkins <ianjerodjenkins@gmail.com>
2018-03-08 21:39:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,15 @@
 def fizzbuzz(n)
-
-
-
+  if(n%5 == 0 && n%3 == 0) 
+    return "Fizzbuzz"
+  end
+  
+  if(n%3 == 0) 
+    return "Fizz"
+  end
+  
+  if(n%5 == 0)
+    return "Buzz"
+  end
 end
+
+fizzbuzz(15)
